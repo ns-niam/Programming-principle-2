da2981e3cf3737a1fc218981cbb31d5646256f5b
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-02-10 08:47:04 +0500

    Create 2.1.py

---
+++ lab2/2.1.py
@@ -0,0 +1,20 @@
+#Python: Booleans
+
+print(100 > 200) #Result: False
+print(100 > 98) #Result: True
+print(9 == 9) #Result: True
+
+#Teory informations that i understand in topic: 
+
+# 1) Any string is True, except empty strings.
+# 2) Any number is True, except 0.
+
+print(bool("Niam")) #Result: True
+print(bool(1234)) #Result: True
+print(bool(0)) #Result: False
+print(bool(0)) #Result: False
+print(bool(0)) #Result: False
+
+x = 2
+y = 2
+print (x is y) #Result: returns True because z is the same number as x
