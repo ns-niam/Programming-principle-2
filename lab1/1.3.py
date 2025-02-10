0bee15729f4e836b49975c32115b3b4f55140f87
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-02-10 08:41:22 +0500

    Create 1.3.py

---
+++ lab1/1.3.py
@@ -0,0 +1,17 @@
+#Python variables and comments
+#In this file legal and illegal type of variables: 
+
+#Legal variables are:
+
+myname = "Niamatullah" #It is my oficcial name :)
+my_name = "Niam" #My classmates call me :) 
+_my_name = "Niam" #close people
+myname = "Niam" #My parents call me like this.
+MYNAME = "Niam" 
+myname = "Niam"
+
+#Illegal variables are: 
+
+2myname = "Niam"
+my-name = "Niam"
+my name = "Niam"
