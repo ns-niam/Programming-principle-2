80fc466190de5236075cda62e99b50eecf66e8ba
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-03-07 09:34:55 +0500

    Update 9.py

--- lab6/9.py
+++ lab6/9.py
@@ -2,7 +2,7 @@
 #Last exercise: Write a Python program to delete file by specified path. Before deleting check for access and whether a given path exists or not.
 
 import os
-first_file = "C:/Users/user/niam/PP2/Lab 6/txt1.file"
+first_file = "C:/Users/user/niam/PP2/Lab 6/file.txt"
 if os.path.exists(first_file):
     if os.access(first_file, os.W_OK):
         os.remove(first_file)
