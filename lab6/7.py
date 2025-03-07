0ac7cdb257a2c18656438a771cf276baef35ce26
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2025-03-07 09:37:07 +0500

    Update 7.py

--- lab6/7.py
+++ lab6/7.py
@@ -1,6 +1,6 @@
 #File handing.
 #Fourth exercise: Write a Python program to count the number of lines in a text file.
 
-a = open("C:/Users/user/niam/PP2/Lab 6/txt.file", "r")
+a = open("C:/Users/user/niam/PP2/Lab 6/file.txt", "r")
 lines = a.readlines()
 print(len(lines))
